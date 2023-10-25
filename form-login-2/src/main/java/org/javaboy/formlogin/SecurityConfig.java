a6446faed10963dc124596122e7042a43cb8eaf2
liy-dv <liy-dv@glodon.com>
2023-10-25 17:51:01 +0800

    login - 2

--- form-login-2/src/main/java/org/javaboy/formlogin/SecurityConfig.java
+++ form-login-2/src/main/java/org/javaboy/formlogin/SecurityConfig.java
@@ -42,24 +42,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login.html")
-                .loginProcessingUrl("/doLogin")
-                .usernameParameter("name")
-                .passwordParameter("passwd")
-                .defaultSuccessUrl("/index")
-                .successForwardUrl("/index")
-                .failureForwardUrl("/f2")
-                .failureUrl("/f1")
+                .loginPage("/login.html")   // 配置 登录页面
+                .loginProcessingUrl("/doLogin") // 配置 登录用户密码请求的url
+                .usernameParameter("name")  // default is username 配置 usernameParameter
+                .passwordParameter("passwd") // default is password 配置 passwordParameter
+                .defaultSuccessUrl("/index")  // 配置 登录成功后跳转的url, 会重定向到登录之前的访问的url
+//                .successForwardUrl("/index") // 配置 登录成功后跳转的url, 只需要配置一个即可
+                .failureForwardUrl("/f2")              // 配置 登录失败后跳转的url, 登录失败之后会发生服务端跳转,只需要配置一个即可,
+//                .failureUrl("/f1")  // 配置 登录失败后跳转的url, 登录失败后 发生重定向
                 .permitAll()
                 .and()
                 .logout()
 //                .logoutUrl("/logout")
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout","POST"))
-                .logoutSuccessUrl("/index")
-                .deleteCookies()
-                .clearAuthentication(true)
-                .invalidateHttpSession(true)
-                .permitAll()
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout","POST"))
+                .logoutSuccessUrl("/index")   // 配置 登出成功后跳转的页面
+                .deleteCookies()  // 配置 登出成功后删除的cookie
+                .clearAuthentication(true) // 配置 登出成功后清除认证信息
+                .invalidateHttpSession(true) // 配置 登出成功后清除session
+                .permitAll()  // 配置 登出页面不需要认证
                 .and()
                 .csrf().disable();
     }
